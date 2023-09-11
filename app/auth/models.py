9cc806e050e1ac78f8da9305a52f020f301a4c48
igor <frakestolstm@gmail.com>
2023-09-11 21:45:01 +0400

    added RefreshSessions model

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import String
+from datetime import datetime
+
+from click import DateTime
+from sqlalchemy import ForeignKey, String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -12,3 +15,11 @@ class User(Base):
 
     def __repr__(self):
         return f"User(id={self.id}, email={self.email})"
+
+
+class RefreshSessions(Base):
+    __tablename__ = "refresh_sessions"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    refresh_token: Mapped[Uuid] = mapped_column(Uuid, unique=True)
+    expire: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)  # type: ignore
