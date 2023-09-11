cbea3fc2355987dbbcffe896f497b58d4d12f67b
igor <frakestolstm@gmail.com>
2023-09-11 23:00:22 +0400

    auth router included to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.auth.router import router as auth_router
+
 app = FastAPI()
+
+app.include_router(auth_router)
+
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
