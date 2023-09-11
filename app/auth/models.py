307a2bb77235b478c967e6d5ffb77770f59f99ea
igor <frakestolstm@gmail.com>
2023-09-11 21:59:52 +0400

    fixede import DateTime

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
-from click import DateTime
-from sqlalchemy import ForeignKey, String, Uuid
+from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint, Uuid
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -22,4 +21,4 @@ class RefreshSessions(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
     refresh_token: Mapped[Uuid] = mapped_column(Uuid, unique=True)
-    expire: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)  # type: ignore
+    expire: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
