79eafead81f59ffea19145371b1def1eb0dabde7
igor <frakestolstm@gmail.com>
2023-09-14 03:32:21 +0400

    added m2m relationships & unique constraint

--- app/task/models.py
+++ app/task/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
@@ -9,14 +9,15 @@ from app.database import Base
 class TaskUser(Base):
     __tablename__ = "tasks_users"
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id", ondelete="CASCADE"))
+    __table_args__ = (UniqueConstraint('user_id', 'task_id'),)
 
 
 class Task(Base):
     __tablename__ = "tasks"
     id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str] = mapped_column(String, nullable=False)
-    deadline: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False
-    )
+    deadline: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+
+    users = relationship("User", secondary="tasks_users", back_populates="tasks")
