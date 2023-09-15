8897eb33019c77cf451a146f57f353f2530b446d
igor <frakestolstm@gmail.com>
2023-09-15 04:42:52 +0400

    added email_users_task_expired

--- app/background_tasks/tasks.py
+++ app/background_tasks/tasks.py
@@ -1,5 +1,9 @@
+import asyncio
+from uuid import UUID
+
 from app.background_tasks.celery_config import celery_app
 from app.services.email_service import EmailService
+from app.task.dao import TaskDAO
 
 
 @celery_app.task
@@ -10,10 +14,10 @@ def email_user_added_to_task(
     user_email: str,
 ):
     email = EmailService()
-    
+
     message = f"Пользователь EMAIL добавил вас как исполнителя задачи: \
             {task_description} Срок выполнения до {deadline}"
-    
+
     subject = f"Новая задача №{task_id}"
 
     msg = email.create_message(
@@ -24,3 +28,26 @@ def email_user_added_to_task(
 
     email.send_email(msg)
 
+
+@celery_app.task()
+def email_users_task_expired(task_id: int):
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(TaskDAO().get_task_with_users(task_id))
+    if result and not result.completed:
+        users = result.users
+        email = EmailService()
+        messages = []
+        for user in users:
+            msg = email.create_message(
+                content=f"Задача {result.id} просрочена!",
+                to=user.email,
+                subject=f"Задача {result.id} просрочена!",
+            )
+            messages.append(msg)
+
+        for message in messages:
+            email.send_email(message)
+
+
+def revoke_task(task_id: UUID):
+    celery_app.control.revoke(str(task_id))
