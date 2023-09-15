7f8a09d20658a7c3604b2c45e463ad1a7f204531
igor <frakestolstm@gmail.com>
2023-09-15 04:41:21 +0400

    Created CeleryTask model

--- app/task/models.py
+++ app/task/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-
-from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint
+from uuid import UUID
+from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -11,7 +11,7 @@ class TaskUser(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
     task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id", ondelete="CASCADE"))
-    __table_args__ = (UniqueConstraint('user_id', 'task_id'),)
+    __table_args__ = (UniqueConstraint("user_id", "task_id"),)
 
 
 class Task(Base):
@@ -19,5 +19,11 @@ class Task(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     description: Mapped[str] = mapped_column(String, nullable=False)
     deadline: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
-
+    completed: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     users = relationship("User", secondary="tasks_users", back_populates="tasks")
+
+
+class CeleryTask(Base):
+    __tablename__ = "celery_tasks"
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id", ondelete="CASCADE"))
