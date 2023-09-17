13408846c46ed3514cec41bf4d224542eb8e32b2
igor <frakestolstm@gmail.com>
2023-09-17 05:06:51 +0400

    changed EmailServer init

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -9,9 +9,13 @@ class EmailService:
         self,
         SMTP_HOST: str = settings.SMTP_HOST,
         SMTP_PORT: int = settings.SMTP_PORT,
+        SMTP_USER: str = settings.SMTP_USER,
+        SMTP_PASSWORD: str = settings.SMTP_PASSWORD,
     ) -> None:
         self.SMTP_HOST = SMTP_HOST
         self.SMTP_PORT = SMTP_PORT
+        self.SMTP_USER = SMTP_USER
+        self.SMTP_PASSWORD = SMTP_PASSWORD
 
     def create_message(
         self, content: str, to: str, subject: str, from_: str = settings.SMTP_USER
@@ -26,5 +30,5 @@ class EmailService:
 
     def send_email(self, message: EmailMessage):
         with smtplib.SMTP_SSL(self.SMTP_HOST, self.SMTP_PORT) as server:
-            server.login(settings.SMTP_USER, settings.SMTP_PASSWORD)
+            server.login(self.SMTP_USER, self.SMTP_PASSWORD)
             server.send_message(message)
